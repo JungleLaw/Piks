ed7fefa37603ea4c36979c4c042bd31713b40ec0
JungleLaw <Junglelaw@live.com>
2016-11-15 16:55:04 +0800

    1#删除未使用布局文件

--- app/src/main/java/com/law/piks/others/AboutActivity.java
+++ app/src/main/java/com/law/piks/others/AboutActivity.java
@@ -13,7 +13,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.alibaba.fastjson.JSON;
 import com.law.piks.R;
